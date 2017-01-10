7fac90037c67de7732fc9c0b4ca343238f2ef91f
Ryan Churchill <rchurchill@box.com>
2017-01-09 16:56:17 -0800

    Adding test for Request Factory post body

--- test/Fixture.php
+++ test/Fixture.php
@@ -4,9 +4,9 @@ namespace Pact\Phpacto;
 
 class Fixture
 {
-    public static function load($fixureName)
+    public static function load($fixtureName)
     {
-        $content = file_get_contents(sprintf(__DIR__.'/fixtures/%s', $fixureName));
+        $content = file_get_contents(sprintf(__DIR__.'/fixtures/%s', $fixtureName));
 
         return $content;
     }

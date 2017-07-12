a170d37b8e66ea72fd2edc10ab8ade2e52aea8c1
Ryan Churchill <rchurchill@box.com>
2017-07-12 16:09:25 -0700

    PactoRequestFactory should append query from request array into the uri string. (PSR-7 request doesn't have its own field for GET query; it's parsed from the URI string.)

--- src/Factory/Pacto/PactoRequestFactory.php
+++ src/Factory/Pacto/PactoRequestFactory.php
@@ -12,11 +12,17 @@ class PactoRequestFactory implements PactoRequestFactoryInterface
     {
         $body = isset($requestArray['body']) ? $requestArray['body'] : '';
 
+	$uriString = $requestArray['path'];
+        if (isset($requestArray['query'])) {
+            $uriString .= '?' . $requestArray['query'];
+        }		
+
         $bodyStream = new Stream('php://memory', 'w');
         $bodyStream->write(json_encode($body));
 
-        $request = new Request(
-            $requestArray['path'],
+        // build the PSR-7 request 
+	$request = new Request(
+            $uriString,
             $requestArray['method'],
             $bodyStream,
             isset($requestArray['headers']) ? $requestArray['headers'] : []
